02555ff72a221eb723e29f45ef44d7a47a91f508
robbiesollie <robbiesollie@gmail.com>
2026-07-27 08:50:34 -0700

    Added Query Parameters to Lucky Number Controller Added import for MapQueryParameter Attribute Added MapQueryParameter Attribute for query parameters into controller function

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 
 class LuckyController extends AbstractController
 {
 	#[Route('/lucky/number', methods: ['GET'])]
-	public function number(int $start = 0, int $end = 100): Response
+	public function number(#[MapQueryParameter] int $start = 0, #[MapQueryParameter] int $end = 100): Response
 	{
 		$number = random_int($start, $end);
 
